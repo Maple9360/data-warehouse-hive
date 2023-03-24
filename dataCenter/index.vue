dbafdf3c68ce1ca299a13df4103b1eaeb8e6ee22
Maple9360 <72908684+Maple9360@users.noreply.github.com>
2023-03-24 11:19:52 +0800

    Add files via upload

---
+++ dataCenter/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+
+</template>
+
+<!-- <script setup lang="ts"> -->
+<script lang="ts">
+
+</script>
+
+
+<!-- <style lang="less" scoped></style> -->
+
+<style  lang="scss" scoped>
+
+.el-header{
+  background-image: url(./background/1280x120-邊框4px.png);
+  width:1280px;
+  height: 300px;
+}
+</style>
