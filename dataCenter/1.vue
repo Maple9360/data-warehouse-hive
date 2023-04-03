b189bde8b44a9090557ac3ac0fdc7f3c5afda94e
Maple9360 <72908684+Maple9360@users.noreply.github.com>
2023-04-03 16:32:24 +0800

    Update 1.vue
    下载

--- dataCenter/1.vue
+++ dataCenter/1.vue
@@ -1,71 +1,84 @@
 <template>
   <div>
-    <el-table :data="tableData" style="width: 100%">
+    <el-table :data="tableData1" ref="table1">
       <el-table-column prop="name" label="姓名"></el-table-column>
       <el-table-column prop="age" label="年龄"></el-table-column>
-      <el-table-column label="操作">
-        <template #default="{ row }">
-          <el-button @click="handleEdit(row)">编辑</el-button>
-        </template>
-      </el-table-column>
+      <el-table-column prop="email" label="邮箱"></el-table-column>
     </el-table>
-    <el-dialog :visible.sync="dialogVisible">
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="姓名">
-          <el-input v-model="form.name"></el-input>
-        </el-form-item>
-        <el-form-item label="年龄">
-          <el-input v-model.number="form.age"></el-input>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="handleSave">确 定</el-button>
-      </div>
-    </el-dialog>
+
+    <button @click="downloadExcel(tableData1)">下载 Excel 文件1</button>
+
+    <el-table :data="tableData2" ref="table2">
+      <el-table-column prop="name" label="姓名"></el-table-column>
+      <el-table-column prop="age" label="年龄"></el-table-column>
+      <el-table-column prop="email" label="邮箱"></el-table-column>
+    </el-table>
+
+    <button @click="downloadExcel(tableData2)">下载 Excel 文件2</button>
+
+    <el-table :data="tableData3" ref="table3">
+      <el-table-column prop="name" label="姓名"></el-table-column>
+      <el-table-column prop="age" label="年龄"></el-table-column>
+      <el-table-column prop="email" label="邮箱"></el-table-column>
+    </el-table>
+
+    <button @click="downloadExcel(tableData3)">下载 Excel 文件3</button>
   </div>
 </template>
 
-<script lang='ts' setup>
+<script lang="ts" setup>
 import { ref } from 'vue';
-import { ElTable, ElTableColumn, ElButton, ElDialog, ElForm, ElFormItem, ElInput } from 'element-plus';
+import * as XLSX from 'xlsx';
 
-interface TableDataItem {
-  name: string;
-  age: number;
-}
+const tableData1 = [...];
+const tableData2 = [...];
+const tableData3 = [...];
 
-const tableData = ref<TableDataItem[]>([
-  { name: '张三', age: 18 },
-  { name: '李四', age: 22 },
-  { name: '王五', age: 25 },
-]);
+const tableRef1 = ref<HTMLElement | null>(null);
+const tableRef2 = ref<HTMLElement | null>(null);
+const tableRef3 = ref<HTMLElement | null>(null);
 
-const form = ref<TableDataItem>({
-  name: '',
-  age: 0,
-});
+const downloadExcel = (tableData: any[]) => {
+  let tableRef: any;
 
-const dialogVisible = ref(false);
-
-function handleEdit(row: TableDataItem) {
-  form.value = { ...row };
-  dialogVisible.value = true;
-}
-
-function handleSave() {
-  const index = tableData.value.findIndex((item) => item.name === form.value.name);
-  if (index > -1) {
-    Object.assign(tableData.value[index], form.value);
-  } else {
-    tableData.value.push(form.value);
+  if (tableData === tableData1) {
+    tableRef = tableRef1;
+  } else if (tableData === tableData2) {
+    tableRef = tableRef2;
+  } else if (tableData === tableData3) {
+    tableRef = tableRef3;
   }
-  dialogVisible.value = false;
-}
-</script>
 
-<style scoped>
-.dialog-footer {
-  text-align: right;
-}
-</style>
+  const table = tableRef.value?.querySelector('.el-table__body');
+
+  if (table) {
+    // 从表格中获取数据
+    const data = [];
+    const rows = table.querySelectorAll('.el-table__row');
+    rows.forEach((row) => {
+      const rowData = [];
+      const cells = row.querySelectorAll('.el-table__cell');
+      cells.forEach((cell) => {
+        rowData.push(cell.textContent);
+      });
+      data.push(rowData);
+    });
+
+    // 将数据转换为 Excel 文件
+    const workbook = XLSX.utils.book_new();
+    const sheet = XLSX.utils.aoa_to_sheet(data);
+    XLSX.utils.book_append_sheet(workbook, sheet, 'Sheet1');
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const blob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+    const fileName = 'table.xlsx';
+
+    // 下载文件
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+};
+</
