4fdcd81576b8a30be0c91acd3e4afce8894b2015
Maple9360 <72908684+Maple9360@users.noreply.github.com>
2023-04-24 13:59:50 +0800

    Update 1.vue
    2

--- dataCenter/1.vue
+++ dataCenter/1.vue
@@ -1,89 +1,39 @@
-import { service } from '@/utils/request.ts'
+<template>
+  <div>
+    <el-select v-model="selectedFields" multiple filterable>
+      <el-option v-for="field in fields" :key="field" :label="field" :value="field" :disabled="selectedFields.indexOf(field) < 0">
+      </el-option>
+    </el-select>
+  </div>
+</template>
 
-// 获取表格数据
-export const getData = () => {
-return service({
-    url: '/DataCenter/getDataCenter',
-    method: 'get',
-})
-}
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+import { ElSelect, ElOption } from 'element-plus';
 
-// 获取表格数据
-export const getSearchData = (table:string,name:string) => {
-    return service({
-        url: '/DataCenter/getSearchData',
-        method: 'get',
-        params:{
-            type:table,
-            site:'',
-            searchName:name
-        }
+const fields = ref<string[]>([]);
+const selectedFields = ref<string[]>([]);
+
+onMounted(() => {
+  // 请求后端获取所有表的字段和数据，并更新fields数组
+  fetch('/api/fields')
+    .then(response => response.json())
+    .then(data => {
+      fields.value = data.fields;
     })
-}
+    .catch(error => {
+      console.error(error);
+    });
+});
 
-// 批量上传
-export const uploadData = (table:string,upload: any) => {
-   
-    return service({
-        url: '/DataCenter/postUploadData',
-        method: 'post',
-        data:table,upload
-    })
-}
-
-// 更新数据
-export const updateData = (table:string, editData:any) => {
-    // editData.add('type',table)
-    editData.type=table
-    return service({
-        url: '/DataCenter/putUpdateData',
-        method: 'put',
-        params:editData,
-    })
-    }
-
-
-// 添加数据
-export const addData = (table:string,addData: any) => {
-    addData.type=table
-return service({
-    url: '/DataCenter/postNewData',
-    method: 'post',
-    params:addData
-})
-}
-
-// 附件上传
-export const addFile = (table:string,fileName:string) => {
-    return service({
-        url: '/DataCenter/postUploadFile',
-        method: 'post',
-        data:table,fileName
-    })
-    }
-
-
-
-// 删除文件
-export const deleteFile = (fileName: string) => {
-    return service({
-        url: '/DataCenter/deleteFile',
-        method: 'put',
-        data: fileName,
-    })
-}
-
-
-// 删除数据
-export const deleteData = (table:string ,dataName: string) => {
-
-    return service({
-        url: '/DataCenter/deleteData',
-        method: 'put',
-        params: {
-            type:table,
-            site:'',
-            fileNumber:dataName,
-        }
-    })
-}
+// 导出组件
+export default {
+  components: { ElSelect, ElOption },
+  setup() {
+    return {
+      fields,
+      selectedFields,
+    };
+  },
+};
+</script>
